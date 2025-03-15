9762a8a2906e947d51154ae7ae6bcfaea628abfb
Stefan Poß <100678744+uELKO@users.noreply.github.com>
2025-03-15 13:58:07 +0100

    Update sen6x.cpp

--- components/sen6x/sen6x.cpp
+++ components/sen6x/sen6x.cpp
@@ -191,7 +191,7 @@ void SEN5XComponent::setup() {
 
       // Finally start sensor measurements
       auto cmd = SEN5X_CMD_START_MEASUREMENTS_RHT_ONLY;
-      if (this->pm_1_0_sensor_ || this->pm_2_5_sensor_ || this->pm_4_0_sensor_ || this->pm_10_0_sensor_) {
+      if (this->pm_1_0_sensor_ || this->pm_2_5_sensor_ || this->pm_4_0_sensor_ || this->pm_10_0_sensor_ || this->pm_0_10_sensor_) {
         // if any of the gas sensors are active we need a full measurement
         cmd = SEN5X_CMD_START_MEASUREMENTS;
       }
